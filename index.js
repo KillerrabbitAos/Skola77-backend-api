29f703ecc6598aea839962731a6f2ec982354d22
Centiljard <89267105+Centiljard@users.noreply.github.com>
2025-02-25 18:25:03 +0100

    format index.js a little

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const createServer = require("./start");
-const logger = require("./logger")
+const logger = require("./logger");
 const config = require("./config/server");
 
 const protocol = config.https ? "https" : "http";
